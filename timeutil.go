5afb0b4df7e5df288155b8b299084b5880182866
mm-chris-adm <mm-chris-adm@mm-admins-MacBook-Pro.local>
2017-04-20 14:02:17 +0800

    Test

--- timeutil.go
+++ timeutil.go
@@ -1,10 +1,6 @@
 package timeutil
 
-import "time"
-
-// Time ...
 type TimeUtil struct {
-	// TZ timezone
 	TZ string
 }
 
@@ -14,29 +10,29 @@ func New(timezone string) TimeUtil {
 	}
 }
 
-// LocalTime get local time given the timezone
-func (timeutil *TimeUtil) LocalTime(timezone string) time.Time {
-	if timezone == "" {
-		panic("Server timezone not set!")
-	}
-
-	serverTime := time.Now()
-	locationTime, _ := time.LoadLocation(timezone)
-	currentTime := serverTime.In(locationTime)
-	return currentTime
-}
-
-// GetCurrentDate Get system's current date
-func (timeutil *TimeUtil) GetCurrentDate(timezone string) string {
-	return LocalTime(timezone).Format("2006-01-02")
-}
-
-// GetCurrentTime Get the system's current date and time
-func (timeutil *TimeUtil) GetCurrentTime(timezone string) string {
-	return LocalTime(timezone).Format("2006-01-02 15:04:05")
-}
-
-// TimeMs Returns Unix time in milliseconds for benchmarking Svc performance.
-func (timeutil *TimeUtil) TimeMs() int64 {
-	return time.Now().UnixNano() / 1000000
-}
+// // LocalTime get local time given the timezone
+// func (timeutil *TimeUtil) LocalTime(timezone string) time.Time {
+// 	if timezone == "" {
+// 		panic("Server timezone not set!")
+// 	}
+//
+// 	serverTime := time.Now()
+// 	locationTime, _ := time.LoadLocation(timezone)
+// 	currentTime := serverTime.In(locationTime)
+// 	return currentTime
+// }
+//
+// // GetCurrentDate Get system's current date
+// func (timeutil *TimeUtil) GetCurrentDate(timezone string) string {
+// 	return LocalTime(timezone).Format("2006-01-02")
+// }
+//
+// // GetCurrentTime Get the system's current date and time
+// func (timeutil *TimeUtil) GetCurrentTime(timezone string) string {
+// 	return LocalTime(timezone).Format("2006-01-02 15:04:05")
+// }
+//
+// // TimeMs Returns Unix time in milliseconds for benchmarking Svc performance.
+// func (timeutil *TimeUtil) TimeMs() int64 {
+// 	return time.Now().UnixNano() / 1000000
+// }
