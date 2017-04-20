70a6e8b37cd041048d718c3f56ebee5eb5513639
mm-chris-adm <mm-chris-adm@mm-admins-MacBook-Pro.local>
2017-04-20 14:15:39 +0800

    Added more functions

--- timeutil.go
+++ timeutil.go
@@ -1,38 +1,45 @@
 package timeutil
 
+import "time"
+
 type TimeUtil struct {
 	TZ string
 }
 
+// New create new TimeUtil instance
 func New(timezone string) TimeUtil {
 	return TimeUtil{
 		TZ: timezone,
 	}
 }
 
-// // LocalTime get local time given the timezone
-// func (timeutil *TimeUtil) LocalTime(timezone string) time.Time {
-// 	if timezone == "" {
-// 		panic("Server timezone not set!")
-// 	}
-//
-// 	serverTime := time.Now()
-// 	locationTime, _ := time.LoadLocation(timezone)
-// 	currentTime := serverTime.In(locationTime)
-// 	return currentTime
-// }
-//
-// // GetCurrentDate Get system's current date
-// func (timeutil *TimeUtil) GetCurrentDate(timezone string) string {
-// 	return LocalTime(timezone).Format("2006-01-02")
-// }
-//
-// // GetCurrentTime Get the system's current date and time
-// func (timeutil *TimeUtil) GetCurrentTime(timezone string) string {
-// 	return LocalTime(timezone).Format("2006-01-02 15:04:05")
-// }
-//
-// // TimeMs Returns Unix time in milliseconds for benchmarking Svc performance.
-// func (timeutil *TimeUtil) TimeMs() int64 {
-// 	return time.Now().UnixNano() / 1000000
-// }
+// GetLocaTime get local time based on timezone. Sets 'Asia/Sinagpore' as the default timezone if not set
+func (tu *TimeUtil) GetLocalTime() time.Time {
+	if tu.TZ == "" {
+		tu.TZ = "Asia/Singapore"
+	}
+
+	serverTime := time.Now()
+	locationTime, _ := time.LoadLocation(tu.TZ)
+	return serverTime.In(locationTime)
+}
+
+// CurrentDate Get system's current date
+func (tu *TimeUtil) CurrentDate() string {
+	return tu.GetLocalTime().Format("2006-01-02")
+}
+
+// CurrentTime Get system's current time
+func (tu *TimeUtil) CurrentTime() string {
+	return tu.GetLocalTime().Format("15:04:05")
+}
+
+// Now Get the system's current date and time
+func (tu *TimeUtil) Now() string {
+	return tu.GetLocalTime().Format("2006-01-02 15:04:05")
+}
+
+// TimeMs Returns Unix time in milliseconds for benchmarking Svc performance.
+func (tu *TimeUtil) TimeMs() int64 {
+	return time.Now().UnixNano() / 1000000
+}
